3738afbf5533046f1cb481ad601259247398bcb4
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-18 12:47:16 +0100

    ADD: Files new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#390EF0",
+        secondary: "#F0BC5E",
+      },
+    },
   },
   plugins: [],
 };
